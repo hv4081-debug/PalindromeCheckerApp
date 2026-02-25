7df057f73054a5402427e9e192cba46b3af66c7f
Sriram474185 <satyasriramprakashpappula2006@gmail.com>
2026-02-25 10:41:36 +0530

    Hrishitha Added UC2 HardCode Palindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,2 +1,14 @@
 public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker AppManagement System");
+        String word = "madam"; // hardcoded
+
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if(word.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
 }
